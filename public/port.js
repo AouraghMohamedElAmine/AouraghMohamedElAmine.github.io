e89ad42fb7ac931081cfa79311a231c7415a0b19
Aouragh Mohamed El Amine <babi_mouh@yahoo.com>
2021-07-25 15:15:36 +0100

    Add files via upload

---
+++ public/port.js
@@ -0,0 +1,56 @@
+
+ let drop= document.getElementById('dropdown'); 
+ let mobile= document.getElementById('mobile-dropdown'); 
+ let arrow= document.getElementById('check'); 
+ let logo= document.getElementById('logo');
+ const projects = document.querySelector('#projects');
+
+ projects.addEventListener('click',()=>{
+    const hero = document.querySelector('#hero'); 
+    const nav = document.querySelector('#nav'); 
+
+   window.scroll({
+      top:hero.clientHeight+nav.clientHeight,
+       behavior: 'smooth'
+    });
+
+ })
+
+ logo.addEventListener('mouseenter',()=>{ 
+
+    let border= document.getElementsByClassName('logo-border');
+    border[0].classList.toggle('logo-border-after');
+
+ })
+
+ logo.addEventListener('mouseleave',()=>{ 
+
+    let border= document.getElementsByClassName('logo-border');
+    border[0].classList.toggle('logo-border-after');
+
+ })
+
+
+drop.addEventListener('change',()=> {   
+mobile.classList.toggle("hidden"); 
+mobile.classList.toggle("is-open");
+mobile.classList.toggle("scale-y-0");
+
+ 
+
+
+     
+ })
+ 
+ 
+ window.addEventListener('scroll',()=>{ 
+     if(mobile.classList.contains("is-open")){
+         drop.dispatchEvent(new Event("change")); 
+         arrow.checked=false;
+
+
+
+
+     }
+   })
+
