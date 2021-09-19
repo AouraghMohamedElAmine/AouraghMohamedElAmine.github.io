242433ecf4951d62486c206d8220eee0505c7183
AouraghMohamedElAmine <babi_mouh@yahoo.com>
2021-09-19 18:03:39 +0100

    v.0.0.1

--- public/port.js
+++ public/port.js
@@ -3,13 +3,21 @@
  let mobile= document.getElementById('mobile-dropdown'); 
  let arrow= document.getElementById('check'); 
  let logo= document.getElementById('logo');
- const svg = document.getElementById('svg')
+ const svg = document.getElementById('svg');
+
  const projectsWebView1 = document.querySelector('.projectsWebView');
  const projectsWebView2 = document.querySelector('.projectsWebView2');
  const projectsMobileView = document.querySelector('.projectsMobileView');
 
+ const SkillsWebView = document.querySelector('.SkillsWebView');
+ const SkillsMobileView = document.querySelector('.SkillsMobileView');
+
+
+ const contactWebView = document.querySelector('.contactWebView');
+ const contactMobileView = document.querySelector('.contactMobileView');
+
+
  const ProjectScroller = ()=>{
-    console.log("clicked");
       const hero = document.querySelector('#hero'); 
       const nav = document.querySelector('#nav'); 
      window.scroll({
@@ -17,10 +25,59 @@
          behavior: 'smooth'
       });
  }
+
+ const SkillsScroller = ()=>{
+   let rect = document.querySelector('.skillsSection').getBoundingClientRect();
+   window.scroll({
+        top: rect.top + window.scrollY,
+        behavior: 'smooth'
+     });
+}
+
+
  
+const SkillsScrollerMobile = ()=>{
+   let rect = document.querySelector('.skillsSection').getBoundingClientRect();
+    
+   window.scroll({
+        top: rect.top + window.scrollY - document.querySelector('.mobile-dropdown').clientHeight,
+        behavior: 'smooth'
+     }); 
+   };
+
+   
+ const ContactScroller = ()=>{
+   let rect = document.querySelector('.contactSection').getBoundingClientRect();
+ 
+   window.scroll({
+        top: rect.top + window.scrollY,
+        behavior: 'smooth'
+     });
+};
+
+ const ContactScrollerMobile = ()=>{
+   let rect = document.querySelector('.contactSection').getBoundingClientRect();
+    
+   window.scroll({
+        top: rect.top + window.scrollY - document.querySelector('.mobile-dropdown').clientHeight,
+        behavior: 'smooth'
+     });
+};
+
+
+
+
+   
  projectsWebView1.addEventListener('click',ProjectScroller);
  projectsWebView2.addEventListener('click',ProjectScroller);
  projectsMobileView.addEventListener('click',ProjectScroller);
+
+ SkillsWebView.addEventListener('click',SkillsScroller);
+ SkillsMobileView.addEventListener('click',SkillsScrollerMobile);
+
+ 
+ contactWebView.addEventListener('click',ContactScroller)
+ contactMobileView.addEventListener('click',ContactScrollerMobile)
 
 
 logo.addEventListener('mouseenter',()=>{ 
@@ -46,4 +103,3 @@ mobile.classList.toggle("scale-y-0");
          arrow.checked=false;
      }
    })
-
